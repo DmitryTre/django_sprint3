c426126f42c604995b1fe600e81c2009230ff652
Дмитрий Трегубов <diamonbusiness@gmail.com>
2025-02-16 19:44:58 +0300

    3Blogicum2

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -7,37 +7,67 @@ User = get_user_model()
 
 # Post (Публикация)
 class Post(PublishedModel):
-    title = models.CharField(max_length=256)
-    text = models.TextField()
-    pub_date = models.DateTimeField()
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        help_text='Если установить дату и время в будущем —'
+        'можно делать отложенные публикации.')
     author = models.ForeignKey(
         User,
         blank=False,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name='Автор публикации'
     )
     location = models.ForeignKey(
         'Location',
         blank=False,
         null=True,
         on_delete=models.SET_NULL,
+        verbose_name='Местоположение'
     )
     category = models.ForeignKey(
         'Category',
         blank=False,
         null=True,
-        on_delete=models.SET_NULL
+        on_delete=models.SET_NULL,
+        verbose_name='Категория'
     )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+
+    def __str__(self):
+        return self.title
 
 
 # Category (Тематическая категория)
 class Category(PublishedModel):
     title = models.CharField(max_length=256)
     description = models.TextField()
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(
+        unique=True,
+        help_text='Идентификатор страницы для URL;'
+        ' разрешены символы латиницы, цифры, дефис и подчёркивание.')
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
 
 
 # Location (Географическая метка)
 class Location(PublishedModel):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, verbose_name='Название места')
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
+
+    def __str__(self):
+        return self.title
 
 # User (Пользователь, эту модель описывать не нужно, она встроена в Django)
