113e2c19b609f564a1e7ee44c48c99918d2f6576
Дмитрий Трегубов <diamonbusiness@gmail.com>
2025-03-04 19:56:12 +0300

    3BlogicumRev2

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -6,7 +6,19 @@ from django.contrib.auth.models import Group
 
 from .models import Category, Location, Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+
+
 admin.site.register(Location)
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.unregister(User)
+
+
+@admin.display(description='Кол-во постов у пользователя')
+def posts_count(self, obj):
+    return obj.posts.count()
