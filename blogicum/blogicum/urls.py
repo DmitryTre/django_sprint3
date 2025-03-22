4c54f2479448b478727447e332a77095f596fda4
Дмитрий Трегубов <diamonbusiness@gmail.com>
2025-03-22 13:54:31 +0300

    static1

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('pages/', include('pages.urls')),
 ]
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
